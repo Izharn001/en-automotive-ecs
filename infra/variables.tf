b32712a511b72725f4d6e2aa2b23d8c50a7a7deb
Izharn001 <izharn786@yahoo.co.uk>
2026-07-20 16:14:56 +0100

    Add Terraform dev variables for CI

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,31 +1,37 @@
 variable "project_name" {
   description = "Name of the project"
   type        = string
+  default     = "en-automotive"
 }
 
 variable "environment" {
   description = "Deployment environment"
   type        = string
+  default     = "dev"
 }
 
 variable "aws_region" {
   description = "AWS region"
   type        = string
+  default     = "eu-west-2"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for the public subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "availability_zones" {
   description = "Availability zones for the public subnets"
   type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "target_group_port" {
@@ -35,15 +41,18 @@ variable "target_group_port" {
 }
 
 variable "ecs_task_cpu" {
-  type = string
+  type    = string
+  default = "256"
 }
 
 variable "ecs_task_memory" {
-  type = string
+  type    = string
+  default = "512"
 }
 
 variable "ecs_service_desired_count" {
-  type = number
+  type    = number
+  default = 1
 }
 
 variable "ecr_image_tag" {
@@ -63,6 +72,7 @@ variable "log_retention_in_days" {
 variable "github_repository" {
   description = "GitHub repository in owner/repository format"
   type        = string
+  default     = "Izharn001/en-automotive-ecs"
 }
 
 variable "github_branch" {
@@ -74,4 +84,5 @@ variable "github_branch" {
 variable "terraform_state_bucket_arn" {
   description = "ARN of the Terraform state bucket"
   type        = string
+  default     = "arn:aws:s3:::en-automotive-terraform-state"
 }
