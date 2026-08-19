4118d6b43d2b4edaefccabda9b3ac5d403c88e30
Izharn001 <izharn786@yahoo.co.uk>
2026-08-19 12:18:37 +0100

    ci: add TFLint checks

--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -8,16 +8,6 @@ variable "tags" {
   type        = map(string)
 }
 
-variable "github_repository" {
-  description = "GitHub repository in owner/repository format"
-  type        = string
-}
-
-variable "github_branch" {
-  description = "GitHub branch allowed to assume the deployment role"
-  type        = string
-  default     = "main"
-}
 
 variable "terraform_state_bucket_arn" {
   type = string
--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -1,8 +1,3 @@
-variable "project_name" {
-  description = "Project name"
-  type        = string
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
